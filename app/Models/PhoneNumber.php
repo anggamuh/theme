eebc88b2985b3482d17003771e462f9888791fae
wildan <wildansuhendar87@gmail.com>
2025-06-12 10:06:21 +0700

    error

--- app/Models/PhoneNumber.php
+++ app/Models/PhoneNumber.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class PhoneNumber extends Model
 {
     use HasFactory;
+    protected $fillable = ['no_tlp'];
     public function article()
     {
         return $this->hasMany(ArticleShow::class);
